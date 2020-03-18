991fe7c14369a0135aa81caef985e42a016ef19d
Boris Arzentar <borisarzentar@gmail.com>
2020-03-18 14:44:03 +0100

    feat(playground): add standard-version

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const fs = require('fs');
-const conventionalChangelog = require('conventional-changelog');
+#!/usr/bin/env node
 
-const changelogFile = fs.createWriteStream('CHANGELOG.md');
+const standardVersion = require('standard-version');
+const semver = require('semver');
+const args = require('yargs')
+  .option('firstRelease', {
+    type: 'boolean',
+    default: false,
+    description: 'If it is first release ever',
+  })
+  .option('fixed', {
+    default: null,
+    type: 'string',
+    description: 'Specific version to release',
+  })
+  .argv;
 
-conventionalChangelog({
-  preset: 'angular'
+const firstRelease = args.firstRelease;
+const specificVersion = semver.valid(args.fixed);
+
+standardVersion({
+  firstRelease,
+  silent: true,
+  noVerify: true,
+  preset: 'angular',
+  header: 'git-playground - Releases\n',
+  infile: 'CHANGELOG.md',
+  releaseAs: specificVersion,
 })
-  .pipe(changelogFile);
+  .then(() => {
+    process.exit(0);
+  });
